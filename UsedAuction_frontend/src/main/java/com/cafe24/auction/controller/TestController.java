ab6cc7daa5b1921dc3d941e04d54e373013d5891
김석현 <@DESKTOP-82CIIBK>
2021-05-14 18:01:38 +0900

    cart 레디스 테스트

--- UsedAuction_frontend/src/main/java/com/cafe24/auction/controller/TestController.java
+++ UsedAuction_frontend/src/main/java/com/cafe24/auction/controller/TestController.java
@@ -1,22 +1,38 @@
 package com.cafe24.auction.controller;
+import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.cafe24.auction.library.libCart;
+import com.cafe24.auction.library.libRedis;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+
 @Controller
 @RequestMapping("/test")
 public class TestController {
 
+	@Autowired
+	private libRedis redis;
+	
+	@Autowired
+	private libCart cart;
+	
 	@GetMapping("")
 	public ModelAndView test() {
 		ModelAndView modelAndView = new ModelAndView();
@@ -37,4 +53,83 @@ public class TestController {
 		return "photo";
 	}
 	
+	/**
+	 * 페이지 조회 수 카운트 하는 컨트롤러
+	 * cookie 를 이용하여 중복 카운트 방지.
+	 * @param request
+	 * @param response
+	 * @return
+	 */
+	@ResponseBody
+	@GetMapping("/page/count")
+	public String pageVisitCount(HttpServletRequest request, HttpServletResponse response) {
+		
+		String today = new SimpleDateFormat("yyyyMMdd").format(new Date());
+		
+		Cookie[] cookies = request.getCookies();
+		String id = "skok1025";
+		
+		// 비교하기 위해 새로운 쿠키
+        Cookie viewCookie = null;
+        
+        // 쿠키가 있을 경우 
+        if (cookies != null && cookies.length > 0) 
+        {
+            for (int i = 0; i < cookies.length; i++)
+            {
+                // Cookie의 name이 cookie + reviewNo와 일치하는 쿠키를 viewCookie에 넣어줌 
+                if (cookies[i].getName().equals("first_visit"))
+                { 
+                    System.out.println("처음 쿠키가 생성한 뒤 들어옴. (already visit)");
+                    viewCookie = cookies[i];
+                    break;
+                }
+            }
+        }
+        
+        if (viewCookie == null) {    
+            System.out.println("cookie 없음");
+            // 쿠키 생성
+            Cookie newCookie = new Cookie("first_visit", "val");
+            response.addCookie(newCookie);
+            
+            redis.incr("test_page_count");
+            
+            // 특정 회원(id) 의 해당날짜 페이지 접근 횟수 
+            redis.hincrBy(
+            	"daily:click:" + id, 
+            	today,
+            	1
+            );
+            
+        }
+        
+        String result = "전체 count : " + redis.get("test_page_count");
+    	
+        Set<String> dateList = redis.hkeys("daily:click:" + id);
+        
+        for (String date : dateList) {
+        	String idDateCount = String.format("- %s 의 %s 페이지 접근 횟수 : %s", id, date, redis.hget("daily:click:" + id, date));
+        	result += "<br>" + idDateCount;
+		}
+		
+		return result;
+	}
+	
+	@ResponseBody
+	@GetMapping("/json/get")
+	public String getJsonString() {
+		JsonObject obj = new JsonObject();
+		obj.addProperty("name", "kim");
+		obj.addProperty("age", "29");
+	
+		return new Gson().toJson(obj);
+	}
+	
+	@ResponseBody
+	@GetMapping("/basket/add")
+	public String getBasketAdd() {
+		cart.addProduct("1", "235", "test product", 1);
+		return cart.getProductList("1").toJSONString();
+	}
 }
--- UsedAuction_frontend/src/main/java/com/cafe24/auction/controller/TestRedisController.java
+++ UsedAuction_frontend/src/main/java/com/cafe24/auction/controller/TestRedisController.java
@@ -1,6 +1,7 @@
 package com.cafe24.auction.controller;
 
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 import org.apache.commons.pool2.impl.GenericObjectPool;
@@ -10,9 +11,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.cafe24.auction.library.libRedis;
+import com.cafe24.auction.library.libRedis2;
 import com.cafe24.auction.service.RedisService;
 
 import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
 
 @Controller
 @RequestMapping("/redis")
@@ -20,6 +24,9 @@ public class TestRedisController {
 
 	@Autowired
 	private RedisService service;
+	
+	@Autowired
+	private libRedis libredis;
 	
 	@ResponseBody
 	@GetMapping("/set")
@@ -42,4 +49,42 @@ public class TestRedisController {
 		
 		return jedis.get("key1");
 	}
+	
+	@ResponseBody
+	@GetMapping("/insert")
+	public String redisInsertTest() {
+		final float TOTAL_OP = 10000f;
+		JedisPool pool = new JedisPool("localhost", 6379);
+		Jedis jedis = pool.getResource();
+		String key, value;
+		long start = System.currentTimeMillis();
+		long loopTime = System.currentTimeMillis();
+		
+		for(int i = 1; i <= TOTAL_OP; i++) {
+			key = value = String.valueOf("key" + (10000 + i));
+			jedis.set(key, value);
+			//jedis.del(key);
+		}
+		
+		long elapsed = System.currentTimeMillis() - start;
+		System.out.println("초당 처리 건수 : " + TOTAL_OP / elapsed * 1000f);
+		System.out.println("소요 시간 : " + elapsed / 1000f + "초");
+		jedis.disconnect();
+		
+		return "end";
+	}
+	
+	@ResponseBody
+	@GetMapping("/libTest")
+	public String redisLibTest() {
+//		Jedis jedis = libRedis2.getInstance().getConnection();
+//		jedis.set("libKey", "libVal");
+//		
+//		return jedis.get("libKey");
+		
+		libredis.set("lib_key", "lib_val");
+		
+		return libredis.get("lib_key");
+	}
+	
 }
