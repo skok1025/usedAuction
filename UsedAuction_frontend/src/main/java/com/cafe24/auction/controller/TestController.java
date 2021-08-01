19758ce11b919e6317a02329e6484d461db993b2
admin <admin@DESKTOP-6FJ1ABV>
2021-08-01 22:27:28 +0900

    기본 탬플릿 세팅

--- UsedAuction_frontend/src/main/java/com/cafe24/auction/controller/TestController.java
+++ UsedAuction_frontend/src/main/java/com/cafe24/auction/controller/TestController.java
@@ -13,6 +13,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -46,6 +47,13 @@ public class TestController {
         modelAndView.addObject("data", map);
         
         return modelAndView;
+	}
+	
+	@GetMapping("/sample")
+	public String sample(Model model) {
+		model.addAttribute("name", "kim");
+		
+        return "sample/index";
 	}
 	
 	@GetMapping("/template")
