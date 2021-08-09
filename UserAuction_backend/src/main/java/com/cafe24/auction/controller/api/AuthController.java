c258f136f27bd5f09f5d60dd2bc8fc6c3074e1e2
shkim <skok1025@naver.com>
2021-08-09 21:17:38 +0900

    _

--- UserAuction_backend/src/main/java/com/cafe24/auction/controller/api/AuthController.java
+++ UserAuction_backend/src/main/java/com/cafe24/auction/controller/api/AuthController.java
@@ -1,9 +1,12 @@
 package com.cafe24.auction.controller.api;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,6 +18,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.cafe24.auction.dto.SecuritySubjectDTO;
 import com.cafe24.auction.service.SecurityService;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -24,10 +31,9 @@ public class AuthController {
 
 	@ResponseBody
 	@PostMapping("/token") 
-	public String generateToken(@RequestBody SecuritySubjectDTO securitySubjectDTO) { 
-	    String userId = securitySubjectDTO.getUserId();
-	    String userPassword = securitySubjectDTO.getUserPassword();
-	    String subject = userId + "::" + userPassword;
+	public String generateToken(@RequestBody SecuritySubjectDTO securitySubjectDTO) throws JsonProcessingException { 
+	    ObjectMapper mapper = new ObjectMapper();
+	    String subject = mapper.writeValueAsString(securitySubjectDTO);
 	    
 		String token = securityService.createToken(subject, 1000 * 60 * 60 * 2L); // 2시간 
 		
@@ -35,9 +41,12 @@ public class AuthController {
 	}
 	
 	@GetMapping("/subject") 
-	public String getSubject(@RequestHeader String AccessToken) { 
+	public SecuritySubjectDTO getSubject(@RequestHeader String AccessToken) throws JsonParseException, JsonMappingException, IOException { 
 		String subject = securityService.getSubject(AccessToken); 
-		return subject; 
+		ObjectMapper mapper = new ObjectMapper();
+		SecuritySubjectDTO dto = mapper.readValue(subject, SecuritySubjectDTO.class);
+		
+		return dto; 
 	}
 
 
